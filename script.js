02687e16c5bd0b55d073ec492982d52a7c7213c3
cheru <30118310+DennoCheru@users.noreply.github.com>
2025-10-15 20:31:49 +0300

    Add event listeners

--- script.js
+++ script.js
@@ -59,4 +59,6 @@ function playGame() {
     }
 }
 
-playGame();
+rockButton.addEventListener('click', () => playRound('rock'));
+paperButton.addEventListener('click', () => playRound('paper'));
+scissorsButton.addEventListener('click', () => playRound('scissors'));
